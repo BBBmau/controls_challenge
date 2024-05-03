339bd1378820a36ccd471cab2308a0675df2fe54
BBBmau <alvarez.mauriciotm@gmail.com>
2024-05-03 15:49:30 -0700

    initial TensorFlow environment setup

--- controllers.py
+++ controllers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class BaseController:
   def update(self, target_lataccel, current_lataccel, state):
     raise NotImplementedError
@@ -7,10 +9,18 @@ class OpenController(BaseController):
   def update(self, target_lataccel, current_lataccel, state):
     return target_lataccel
 
-
 class SimpleController(BaseController):
   def update(self, target_lataccel, current_lataccel, state):
     return (target_lataccel - current_lataccel) * 0.3
+  
+class Agent(BaseController):
+  def update(self, target_lataccel, current_lataccel, state):
+    # Create a default RandomState
+    rng = np.random.default_rng()
+
+    # Generate a random float32 between -2 and 2
+    random_steering_action = rng.uniform(-2, 2, dtype=np.float32)   
+    return random_steering_action
   
 # State = namedtuple('State', ['roll_lataccel', 'v_ego', 'a_ego'])
 class MauController(BaseController):
@@ -22,9 +32,9 @@ class MauController(BaseController):
         return 0.1
       return 0
 
-
 CONTROLLERS = {
   'open': OpenController,
   'simple': SimpleController,
   'mau' : MauController,
+  'learning_agent' : Agent,
 }
